f19bac8e7cd05ac7504f6248076870bccf9e9bc3
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 00:38:19 -0700

    Feat: create bachelorette with #contestant_avg_age and #contestants_hometowns

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -1,4 +1,16 @@
-class Bachelorette <ApplicationRecord
+class Bachelorette < ApplicationRecord
   has_many :contestants
-  
+  has_many :contestant_outings, through: :contestants  
+  has_many :outings, through: :contestant_outings
+
+  validates_presence_of :name, :season_number, :desc
+
+  def contestant_avg_age
+    return 0 if contestants.count == 0
+    contestants.average(:age)
+  end
+
+  def contestants_hometowns
+    contestants.distinct.pluck(:hometown)
+  end
 end
