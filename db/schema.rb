0adebb1dd1e0bc5acd31748bef2dd48397c2ad09
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 00:42:39 -0700

    Feat: update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_052320) do
+ActiveRecord::Schema.define(version: 2022_01_13_205324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2019_12_20_052320) do
     t.integer "season_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "desc"
+  end
+
+  create_table "contestant_outings", force: :cascade do |t|
+    t.bigint "contestant_id"
+    t.bigint "outing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contestant_id"], name: "index_contestant_outings_on_contestant_id"
+    t.index ["outing_id"], name: "index_contestant_outings_on_outing_id"
   end
 
   create_table "contestants", force: :cascade do |t|
@@ -32,5 +42,15 @@ ActiveRecord::Schema.define(version: 2019_12_20_052320) do
     t.index ["bachelorette_id"], name: "index_contestants_on_bachelorette_id"
   end
 
+  create_table "outings", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "contestant_outings", "contestants"
+  add_foreign_key "contestant_outings", "outings"
   add_foreign_key "contestants", "bachelorettes"
 end
