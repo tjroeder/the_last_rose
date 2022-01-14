976360f8d5795790d7240ec3156ea865baebc763
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 00:39:28 -0700

    Feat: create contestant model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,7 @@
-class Contestant <ApplicationRecord
+class Contestant < ApplicationRecord
   belongs_to :bachelorette
-  
+  has_many :contestant_outings
+  has_many :outings, through: :contestant_outings
+
+  validates_presence_of :name, :age, :hometown
 end
