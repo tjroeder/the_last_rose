d46ccb399486cd0f096073f4d3dbd8b20057b554
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 00:35:44 -0700

    Feat: create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bachelorettes, only: [:show], as: 'bach' do
+    resources :contestants, only: [:index], as: 'cont', controller: 'bachelorette_contestants'
+  end
+  
+  resources :contestants, only: [:show]
+  resources :outings, only: [:show]
 end
